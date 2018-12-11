d7d0ac86e04974e8ba5be214b52cb62482c810ca
Jiajun Wang <jjwang@linkedin.com>
2018-12-10 21:48:58 -0800

    Add various operational metrics
    Add the following metrics:
    * Max/Min/Avg latency for read and write requests.
    * Total request count for read and write requests.
    * Connection/session create and close count.

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServerBean.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServerBean.java
@@ -150,10 +150,27 @@ public class ZooKeeperServerBean implements ZooKeeperServerMXBean, ZKMBeanInfo {
         serverStats.resetRequestCounters();
         serverStats.resetLatency();
         serverStats.resetFsyncThresholdExceedCount();
+        serverStats.resetConnectionCounters();
     }
 
     public long getNumAliveConnections() {
         return zks.getNumAliveConnections();
+    }
+
+    public long getConnectionCreateCount() {
+        return zks.serverStats().getConnectionCreateCount();
+    }
+
+    public long getConnectionCloseCount() {
+        return zks.serverStats().getConnectionCloseCount();
+    }
+
+    public long getSessionCloseCount() {
+        return zks.serverStats().getSessionCloseCount();
+    }
+
+    public long getSessionCreateCount() {
+        return zks.serverStats().getSessionCreateCount();
     }
 
     @Override
@@ -406,5 +423,45 @@ public class ZooKeeperServerBean implements ZooKeeperServerMXBean, ZKMBeanInfo {
 
     public int getMaxCnxns() {
         return ServerCnxnHelper.getMaxCnxns(zks.secureServerCnxnFactory, zks.serverCnxnFactory);
+    }
+
+    @Override
+    public long getMinReadRequestLatency() {
+        return zks.serverStats().getMinReadLatency();
+    }
+
+    @Override
+    public long getAvgReadRequestLatency() {
+        return zks.serverStats().getAvgReadLatency();
+    }
+
+    @Override
+    public long getMaxReadRequestLatency() {
+        return zks.serverStats().getMaxReadLatency();
+    }
+
+    @Override
+    public long getMinWriteRequestLatency() {
+        return zks.serverStats().getMinWriteLatency();
+    }
+
+    @Override
+    public long getAvgWriteRequestLatency() {
+        return zks.serverStats().getAvgWriteLatency();
+    }
+
+    @Override
+    public long getMaxWriteRequestLatency() {
+        return zks.serverStats().getMaxWriteLatency();
+    }
+
+    @Override
+    public long getReadRequestCount() {
+        return zks.serverStats().getReadCount();
+    }
+
+    @Override
+    public long getWriteRequestCount() {
+        return zks.serverStats().getWriteCount();
     }
 }
